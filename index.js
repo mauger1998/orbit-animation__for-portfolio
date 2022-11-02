cd2034f51feb267ab40e837180f4aaf7c0ef57de
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-02 21:28:21 +0000

    Make animation fade in on scroll then start

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const firstTarget = document.querySelector("ul")
 const secondTarget = Array.from(document.querySelectorAll("#orbitImg"))
 const thirdTarget = Array.from(document.querySelectorAll("p"))
 const wholeAnimation = document.querySelector(".orbit__body")
+const firstList = document.querySelector("#firstList")
+const secondList = document.querySelector("#secondList")
+const thirdList = document.querySelector("#thirdList")
+const fourthList = document.querySelector("#fourthList")
+const fifthList = document.querySelector("#fifthList")
+const sixthList = document.querySelector("#sixthList")
+const seventhList = document.querySelector("#seventhList")
+const eigthList = document.querySelector("#eigthList")
+
 
 
 
 const options = { 
     root: null,
     rootMargin: " -150px",
-    threshold: 0.25,
+    threshold: 0.4,
 }
 
 const observer = 
@@ -24,6 +33,15 @@ const observer =
                     thing.classList.add("addToP")
                 })
                 wholeAnimation.style.opacity = "1"
+                firstList.classList.add("addToFirstList")
+                secondList.classList.add("addToSecondList")
+                thirdList.classList.add("addToThirdList")
+
+                fourthList.classList.add("addToFourthList")
+                fifthList.classList.add("addToFifthList")
+                sixthList.classList.add("addToSixthList")
+                seventhList.classList.add("addToSeventhList")
+                eigthList.classList.add("addToEigthList")
 
 
 
